28782f09472dec68b1ab96710cfe02b039a2c82a
Nicklas Klinch <nicklasklinch@gmail.com>
2016-04-26 15:15:49 +0200

    GUI - added canvas, text UI and scripts

---
+++ Assets/General/Scripts/UIDisplayRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIDisplayRotation : MonoBehaviour {
+    public UnityEngine.UI.Text displayRotationX;
+    public UnityEngine.UI.Text displayRotationY;
+    public UnityEngine.UI.Text displayRotationZ;
+    private Transform tf;
+
+    // Use this for initialization
+    void Start () {
+        tf = GameObject.Find("Cube").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get transform.rotation and store
+        displayRotationX.text = tf.rotation.x.ToString();
+        displayRotationY.text = tf.rotation.x.ToString();
+        displayRotationZ.text = tf.rotation.x.ToString();
+    }
+}
---
+++ Assets/General/Scripts/UIDisplayVelocity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIDisplayVelocity : MonoBehaviour {
+    public UnityEngine.UI.Text displayVelocityX;
+    public UnityEngine.UI.Text displayVelocityY;
+    public UnityEngine.UI.Text displayVelocityZ;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GameObject.Find("Cube").GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Only works if gameobject can modify its velocities in the x-, y- and z axes
+        //get velocities, update floats, update display
+        displayVelocityX.text = rb.velocity.x.ToString();
+        displayVelocityX.text = rb.velocity.y.ToString();
+        displayVelocityX.text = rb.velocity.z.ToString();
+    }
+}

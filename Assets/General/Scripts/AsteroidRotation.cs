171d09105bc9f11ab44fea2210f1aa35e3031d96
Northstorm <nico_sand@live.dk>
2016-05-03 17:27:00 +0200

    Added roration to the orbits and asteroids

---
+++ Assets/General/Scripts/AsteroidRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidRotation : MonoBehaviour
+{
+    int RorationAngleX;
+    int RorationAngleY;
+    int RorationAngleZ;
+    // Use this for initialization
+    void Start ()
+    {
+        RorationAngleX = Random.Range(4, 10);
+        RorationAngleY = Random.Range(4, 10);
+        RorationAngleZ = Random.Range(4, 10);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.eulerAngles += new Vector3(RorationAngleX, RorationAngleY, 0) * Time.deltaTime;
+    }
+}
---
+++ Assets/General/Scripts/AstroidBeltOrbit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroidBeltOrbit : MonoBehaviour {
+    public GameObject[] asteroidBelts = new GameObject[3];
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        asteroidBelts[0].transform.eulerAngles += new Vector3(0, 1.50f, 0) * Time.deltaTime;
+        asteroidBelts[1].transform.eulerAngles += new Vector3(0, 1, 0) * Time.deltaTime;
+        asteroidBelts[2].transform.eulerAngles += new Vector3(0, 0.50f, 0) * Time.deltaTime;
+    }
+}

d8fb50b2d4873057b51e9e9dfccf9f47429aef2a
MagdalenaH <magdalena.houska@gmail.com>
2016-05-11 13:01:00 +0200

    Working with sounds

--- Assets/General/Scripts/Sounds.cs
+++ Assets/General/Scripts/Sounds.cs
@@ -3,65 +3,77 @@ using System.Collections;
 
 public class Sounds : MonoBehaviour {
 
-    public AudioClip speedUp;
     public AudioClip powerEngine;
-    public AudioClip slowDown;
     public AudioClip ambient;
-    private AudioSource accelleration;
-    private AudioSource deaccelleration;
     private AudioSource engineSource;
     private AudioSource ambientSource;
-
-
-    void Awake()
-    {
-
+    private float engineVolume;
+    private float maxVolume;
+    private float velocity;
+    private Rigidbody rb;
+    private float accelerating;
+    
+	// Use this for initialization
+	void Start () {
         ambientSource = gameObject.AddComponent<AudioSource>();
-        accelleration = gameObject.AddComponent<AudioSource>();
-        deaccelleration = gameObject.AddComponent<AudioSource>();
         engineSource = gameObject.AddComponent<AudioSource>();
 
         ambientSource.loop = true;
         ambientSource.playOnAwake = true;
+
         ambientSource.clip = ambient;
         engineSource.clip = powerEngine;
-        deaccelleration.clip = slowDown;
-        accelleration.clip = speedUp;
-    }
-
-	// Use this for initialization
-	void Start () {
         ambientSource.Play();
+        engineVolume = 0;
+        maxVolume = 20f;
+        velocity = 0;
+        rb = GameObject.Find("cockpit").GetComponent<Rigidbody>(); 
     }
 
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (!(accelleration.isPlaying || deaccelleration.isPlaying))
+          float velocityX = Mathf.Round(rb.velocity.x);
+          float velocityY = Mathf.Round(rb.velocity.y);
+          float velocityZ = Mathf.Round(rb.velocity.z);
+          velocity = Mathf.Sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
+        
+        if (velocity > accelerating)
         {
-            ambientSource.PlayOneShot(ambient, 0.03f);
-            ambientSource.loop = true;
+            if (!engineSource.isPlaying)
+                engineSource.Play();
+
+            else
+                FadeIn();
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (deaccelleration.isPlaying)
-                deaccelleration.Stop();
 
-            transform.position += Vector3.up * 1.5f * Time.deltaTime;
-
-            if (!accelleration.isPlaying)
-            {
-                accelleration.PlayOneShot(powerEngine, 0.5f);
-            }
+        else {
+            if (engineSource.isPlaying)
+               FadeOut();
         }
-     
-        if(Input.GetKeyUp(KeyCode.UpArrow))
-        {
-            if (accelleration.isPlaying)
-                accelleration.Stop();
+    }
 
-            deaccelleration.PlayOneShot(slowDown, 1f);
-        }   
+    void FadeIn()
+    {
+        if (engineVolume <= maxVolume)
+            engineVolume += 1f * Time.deltaTime;
+
+        engineSource.volume = engineVolume;
+    }
+
+    void FadeOut()
+    {
+        if (engineVolume > 0.0f && engineVolume < 1.5f)
+            engineSource.Stop();
+        else
+            engineVolume -= 5f * Time.deltaTime;
+
+        engineSource.volume = engineVolume;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 100), "Velocity : " + velocity);
     }
 }

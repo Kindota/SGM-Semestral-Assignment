d263ca64ae9e917158f07b6751bddc666c70b40f
MagdalenaH <magdalena.houska@gmail.com>
2016-05-11 14:49:12 +0200

    Adding sound to rotation

--- Assets/General/Scripts/Sounds.cs
+++ Assets/General/Scripts/Sounds.cs
@@ -9,7 +9,14 @@ public class Sounds : MonoBehaviour {
     private AudioSource ambientSource;
     private Rigidbody rb;
     private float velocity;
-    
+    private float rotation;
+    private Transform tf;
+
+    void Awake()
+    {
+        ambientSource = GetComponent<AudioSource>();
+    }
+
 	// Use this for initialization
 	void Start () {
         
@@ -23,7 +30,7 @@ public class Sounds : MonoBehaviour {
         ambientSource.PlayOneShot(ambient, 1);
         ambientSource.loop = true;
         ambientSource.playOnAwake = true;
-         
+        tf = GameObject.Find("cockpit").GetComponent<Transform>();
     }
 
 	
@@ -35,10 +42,19 @@ public class Sounds : MonoBehaviour {
         float velocityZ = Mathf.Round(rb.velocity.z);
         velocity = Mathf.Sqrt((velocityX * velocityX) + (velocityY * velocityY) + (velocityZ * velocityZ));
 
+        float rotationX = tf.rotation.eulerAngles.x;
+        float rotationY = tf.rotation.eulerAngles.y;
+        float rotationZ= tf.rotation.eulerAngles.z;
+        rotation = Mathf.Sqrt((rotationX * rotationX) + (rotationY * rotationY) + (rotationZ * rotationZ));
+
         if (!engineSource.isPlaying)
                  engineSource.Play();
 
-        engineSource.volume = velocity / 100;
+        if(velocity != 0)
+            engineSource.volume = velocity / 100;
+
+        else
+            engineSource.volume = rotation / 100;
 
     }
 
